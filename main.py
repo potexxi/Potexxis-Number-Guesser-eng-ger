b69045f68ca759edb3e9dca8fa150c125b81489f
potexxi <165912649+potexxi@users.noreply.github.com>
2024-09-23 19:25:53 +0200

    Potexxis Number Guesser
    This programm guesses your number!!!!

---
+++ main.py
@@ -0,0 +1,41 @@
+input ("Wilkommen zu Potexxi's Zahlen Guesser!");
+input ("Welcome to Potexxis's Number Guesser!");
+
+input ("Wählen Sie eine Sprache");
+input ("Choose a language");
+
+x = input ("Type/Tippe english or/oder deutsch: ");
+
+y = 'deutsch'
+u = 'english'
+
+if x == y:
+    z = input ("Gebe eine Zahl ein: ")
+    import time
+    symbols = ['⣾', '⣷', '⣯', '⣟', '⡿', '⢿', '⣻', '⣽']
+    i = 3
+    i = (i + 1) % len(symbols)
+    print('\r\033[K%s Lese deine Gedanken...' % symbols[i], flush=True, end='')
+    time.sleep(5)
+
+    input ("   drücke ENTER, um meine Zahl aufzudecken")
+
+    print (z)
+    input ("Richtig?");
+
+
+if x == u:
+    o = input ("Insert a number: ");
+
+    import time
+
+    symbols = ['⣾', '⣷', '⣯', '⣟', '⡿', '⢿', '⣻', '⣽']
+    i = 3
+    i = (i + 1) % len(symbols)
+    print('\r\033[K%s Read your thoughts...' % symbols[i], flush=True, end='')
+    time.sleep(5)
+
+    input("   press ENTER to reveal my number");
+
+    print(o);
+    input("Right?");
