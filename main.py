57b52026cd8faad969943b89ce5021c7913843da
potexxi <165912649+potexxi@users.noreply.github.com>
2024-09-24 18:29:54 +0200

    Second Commit

--- main.py
+++ main.py
@@ -1,46 +1,49 @@
 
+import time
+print ("Wählen Sie eine Sprache")
+time.sleep(1.5)
 
-input ("Wählen Sie eine Sprache");
-input ("Choose a language");
+import time
+print ("Choose a language")
+time.sleep(1.5)
 
-x = input ("Type/Tippe english or/oder deutsch: ");
+x = input ("Type/Tippe english or/oder deutsch: ")
 
 y = 'deutsch'
 u = 'english'
 
-if x == y:
-    input("Wilkommen zu Potexxi's Zahlen Guesser!");
+if x != y and x != u:
+    import time
+    print ("ERROR!")
+    time.sleep(3)
+
+elif x == y:
+    import time
+    print("Wilkommen zu Potexxi's Zahlen Guesser!")
+    time.sleep(2)
     z = input ("Gebe eine Zahl ein: ")
     import time
     symbols = ['⣾', '⣷', '⣯', '⣟', '⡿', '⢿', '⣻', '⣽']
     i = 3
     i = (i + 1) % len(symbols)
     print('\r\033[K%s Lese deine Gedanken...' % symbols[i], flush=True, end='')
-    time.sleep(4)
-
+    time.sleep(3.5)
     input ("   drücke ENTER, um meine Zahl aufzudecken")
-
     print (z)
-    input ("Richtig?");
+    input ("Richtig?")
 
 
-if x == u:
-    input("Welcome to Potexxis's Number Guesser!");
-    o = input ("Insert a number: ");
-
+elif x == u:
     import time
-
+    print("Welcome to Potexxis's Number Guesser!")
+    time.sleep(2)
+    o = input ("Insert a number: ")
+    import time
     symbols = ['⣾', '⣷', '⣯', '⣟', '⡿', '⢿', '⣻', '⣽']
     i = 3
     i = (i + 1) % len(symbols)
     print('\r\033[K%s Read your thoughts...' % symbols[i], flush=True, end='')
-    time.sleep(4)
-
-    input("   press ENTER to reveal my number");
-
-    print(o);
-    input("Right?");
-
-
-
-
+    time.sleep(3.5)
+    input("   press ENTER to reveal my number")
+    print(o)
+    input("Right?")
